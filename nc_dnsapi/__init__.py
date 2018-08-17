d6d90eebd50a4ece61d48978c2c0b76d38f9bf95
Thomas Gläßle <t_glaessle@gmx.de>
2018-08-17 05:18:03 +0200

    Make the `print(api.dns_records(…))` output more useful

--- nc_dnsapi/__init__.py
+++ nc_dnsapi/__init__.py
@@ -39,6 +39,8 @@ class DNSRecord(object):
     def __str__(self):
         return str(self.__dict__)
 
+    __repr__ = __str__
+
     def __eq__(self, other):
         if not isinstance(other, DNSRecord):
             return False
